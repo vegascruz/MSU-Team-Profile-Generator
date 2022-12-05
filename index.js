b82fd8ac8fe088b8545e7c72fc82ef6faedc719d
Vegascruz <cruzv@students.westerntc.edu>
2022-12-05 16:28:13 -0600

    Added employee prompts... which contains validation for different roles. Also made it so we could add more employees

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var inquirer = require('inquirer');
+const Employee = require('./library/Employee');
 
 //imported classes or 'team profile's ' : NOTE - the employee class isn't being transported,
 //because all the other classes inherit its traits
@@ -76,4 +77,91 @@ function addManager(){
     })
 }
 
-//addManager();
+function addEmployee(){
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'role',
+            message: "What is the employee's role?",
+            choices: ['Engineer', 'Intern']
+        },
+        {
+            type: 'input',
+            name: 'employeeName',
+            message: "Employee Name",
+            validate: employeeName => {
+                if(employeeName){
+                    return true;
+                }else{
+                    console.log('Please enter in a valid name');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'ID',
+            message: "What is the employee's ID",
+            validate: ID =>{
+                if((isNaN(ID)) || (ID == "")){
+                    console.log('Please enter a valid ID');
+                    return false;
+                }else{
+                    return true;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Employee email',
+            validate: email => { //validates that it's a proper email
+                valid = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()\.,;\s@\"]+\.{0,1})+([^<>()\.,;:\s@\"]{2,}|[\d\.]+))$/.test(email);
+                if(valid){
+                    return true;
+                }else{
+                    console.log('\n Please enter in a valid email');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'githubUserName',
+            message: 'Employee github username?',
+            //when is a conditional property... the rest will only run if found true
+            when: (input) => input.role === 'Engineer',
+            validate: githubUserName => {
+                if(githubUserName){
+                    return true;
+                }else{
+                    console.log('Please enter a valid username');
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: 'What is the interns school?',
+            //this will validate if role = intern
+            when: (input) => input.role === 'Intern',
+            validate: school => {
+                if(school){
+                    return true;
+                }else{
+                    console.log("Please enter the school")
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'confirmation',
+            message: 'Would you like to add another teammate?',
+            default: false
+        }
+    ])
+    .then((employeeInput) => {
+
+    })
+}
